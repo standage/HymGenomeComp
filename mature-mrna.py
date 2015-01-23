bf0aa3ebe987ec8bdaa27cfe7dbba3536647437b
Daniel Standage <daniel.standage@gmail.com>
2015-01-22 20:17:32 -0500

    Accommodate additional use case, add CLI

--- mature-mrna.py
+++ mature-mrna.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+import argparse
 import re
 import sys
 
-def exons_to_mature_mrna(fp):
+def mrna_exons(fp, convert=False):
   mrnaids = {}
   for line in fp:
     line = line.rstrip()
@@ -15,11 +16,21 @@ def exons_to_mature_mrna(fp):
     elif fields[2] == "exon":
       parentid = re.search("Parent=([^;\n]+)", fields[8]).group(1)
       if parentid in mrnaids:
-        fields[2] = "mRNA"
-        fields[8] = re.sub("ID=[^;\n]+;*", "", fields[8])
-        fields[8] = fields[8].replace("Parent=", "ID=")
+        if convert:
+          fields[2] = "mRNA"
+          fields[8] = re.sub("ID=[^;\n]+;*", "", fields[8])
+          fields[8] = fields[8].replace("Parent=", "ID=")
+        else:
+          fields[8] = re.sub("Parent=[^;\n]+;*", "", fields[8])
         yield "\t".join(fields)
 
 if __name__ == "__main__":
-  for mrna in exons_to_mature_mrna(sys.stdin):
-    print mrna
+  desc = "Extract mRNA-associated exons"
+  usage = "%(prog)s [-h] [-c] < annot.gff3 > exons.gff3"
+  parser = argparse.ArgumentParser(description=desc, usage=usage)
+  parser.add_argument("-c", "--convert", action="store_true",
+                      help="Convert to mature mRNA multifeatures")
+  args = parser.parse_args()
+
+  for exon in mrna_exons(sys.stdin, args.convert):
+    print exon
